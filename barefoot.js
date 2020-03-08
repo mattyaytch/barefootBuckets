7678ff70c1298ef8438f6c59fca55f3e732641e6
Matty Aytch <matty.aytch@outlook.com>
2020-03-08 22:40:19 +1100

    Added Keypress Function
    Added the keypress function for the enter key to run the calculateBuckets function when pressed.

--- barefoot.js
+++ barefoot.js
@@ -1,5 +1,3 @@
-
-
 function calculateBuckets (){
     var wage = document.getElementById('wage').value;
     var expensesBucket = (60 / 100) * wage;
@@ -7,16 +5,28 @@ function calculateBuckets (){
     var fireExtinguisherBucket = (20 / 100) * wage;
     var smileBucket = (10 / 100) * wage;
     var total = wage;
-    
+
     document.getElementById("expenses").innerHTML = "Daily Expenses: $" + expensesBucket.toFixed(0);
     document.getElementById("splurge").innerHTML = "Splurge: $" + splurgeBucket.toFixed(0);
     document.getElementById("smile").innerHTML = "Smile: $" + smileBucket.toFixed(0);
     document.getElementById("extinguisher").innerHTML = "Fire Extinguisher: $" + fireExtinguisherBucket.toFixed(0);
-       document.getElementById("total").innerHTML = "Total: $" + wage;
+    document.getElementById("total").innerHTML = "Total: $" + wage;
      
     console.log("Expenses Bucket: $" + expensesBucket.toFixed(0));
     console.log("Splurge Bucket: $" + splurgeBucket.toFixed(0));
     console.log("Fire Extinguisher: $" + fireExtinguisherBucket.toFixed(0));
     console.log("Smile Bucket: $" + smileBucket.toFixed(0));
     console.log("Total: $" + total);
+
 }
+
+var input = document.getElementById('wage');
+
+ input.addEventListener("keypress", function(event) {
+    if (input.value > 0 && event.keyCode === 13) {
+        calculateBuckets();
+      }
+    })
+
+
+
